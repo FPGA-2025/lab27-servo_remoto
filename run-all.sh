0240a729d587069a8f7d58a330d2f5fb546a8377
Julio Nunes Avelar <jn5.guape@gmail.com>
2025-05-29 15:30:41 -0300

    Corrigindo run.sh

--- run-all.sh
+++ run-all.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-for a in `seq 0 0`; do
-    ./run.sh $a
-done
+
+# Compila todos os arquivos .v
+iverilog -o tb *.v
+
+# Remove saída anterior
+rm -f saida.out
+
+# Executa o testbench e salva a saída
+./tb > saida.out
+
+# Mostra apenas as linhas com "OK" ou "ERRO"
+grep -oE '\b(OK|ERRO)\b' saida.out
--- run.sh
+++ run.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    echo "Erro: É preciso especificar o número do teste"
-    exit 1
-fi
+
+# Compila todos os arquivos .v
 iverilog -o tb *.v
+
+# Remove saída anterior
 rm -f saida.out
-cp test/teste$1.txt teste.txt
+
+# Executa o testbench e salva a saída
 ./tb > saida.out
-cp saida.out test/saida$1.out
-cp saida.vcd test/saida$1.vcd
-rm saida.out saida.vcd teste.txt
 
-grep -v '\$finish' test/saida$1.out > tmp_filtered.out
-if diff -w tmp_filtered.out test/saida$1.ok >/dev/null; then
-    echo "OK"
-    exit 0
-else
-    echo "ERRO"
-    exit 1
-fi
-
+# Mostra apenas as linhas com "OK" ou "ERRO"
+grep -oE '\b(OK|ERRO)\b' saida.out
